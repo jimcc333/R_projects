2816984bfd6e300e9bb6862e37499dd80464a3a0
jimcc333 <jimcc333@gmail.com>
2018-03-06 16:56:46 -0600

    Adds vectors to the variable types

--- VariableTypes.R
+++ VariableTypes.R
@@ -23,4 +23,29 @@ c <- T
 typeof(c)
 d <- TRUE
 typeof(d)
-  
+
+# Vectors
+NumberVector <- c(3, 4, 5, 6)
+is.numeric(NumberVector)
+is.integer(NumberVector)
+is.double(NumberVector)
+
+IntegerVector <- c(3L, 4L,88L)
+is.numeric(IntegerVector)
+is.integer(IntegerVector)
+is.double(IntegerVector)
+
+CharacterVector <- c("a", "B", "hello", "world")
+is.character(CharacterVector)
+
+seq() # Sequence, like ":"
+rep() # Replicate
+
+SequenceVector <- seq(1,16,2)
+SequenceVector
+
+RepeatVector <- rep(3.14, 20)
+RepeatVector
+
+MetaRepeat <- rep(SequenceVector, 5)
+MetaRepeat
