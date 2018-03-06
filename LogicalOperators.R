11325e11ba26913a6f44b6d26668a9c944c07880
Cem <jimcc333@gmail.com>
2018-03-06 13:19:19 -0600

    Adds script to survey logical operators

---
+++ LogicalOperators.R
@@ -0,0 +1,26 @@
+# Logical Operators
+
+# <, less than
+1 < 2
+
+# >, greater than
+1 > 2
+
+# ==, equals
+1 == 1
+2 == 3
+
+# !=, not equal
+2 != 3
+
+# !, not
+result <- !(4<2)
+
+# |, or
+result | F
+
+# &, and
+result & F
+
+# isTRUE, true check
+isTRUE(result)
