c38ad30991b366427e96b944257ff97a94596fa4
Cem <jimcc333@gmail.com>
2018-03-06 13:23:22 -0600

    Adds script to use while loop and keyboard stop

---
+++ WhileLoop.R
@@ -0,0 +1,14 @@
+# While loop
+
+# Check keyboard stop using escape key
+while(T){
+  print("Hit escape key!")
+}
+
+# Count up to value
+count_to <- 20
+counter <- 1
+while(counter < count_to + 1){
+  print(counter)
+  counter <- counter + 1
+}
