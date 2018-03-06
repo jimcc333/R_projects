bda5e2a9aa759376c61cfb4d1064da00fa1605eb
Cem <jimcc333@gmail.com>
2018-03-06 13:27:52 -0600

    Adds the loops script

---
+++ Loops.R
@@ -0,0 +1,24 @@
+# LOOPS
+
+
+# ----- while loop -----
+
+# Check keyboard stop using escape key
+while(T){
+  print("Hit escape key!")
+}
+
+# Count up to value
+count_to <- 20
+counter <- 1
+while(counter < count_to + 1){
+  print(counter)
+  counter <- counter + 1
+}
+
+
+# ----- for loop -----
+count_to <- 20
+for(i in 1:count_to){
+  print(i)
+}
