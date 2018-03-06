bd2313584147f423dfef729db89a0089bec2fdee
Cem <jimcc333@gmail.com>
2018-03-06 13:07:14 -0600

    Adds script to survey variable types

---
+++ VariableTypes.R
@@ -0,0 +1,25 @@
+# Quick survey of data types in R
+
+# Integer
+x <- 2L
+typeof(x) # Confrm
+
+# Double
+y <- 2.
+typeof(y)
+
+# Complex
+z <- 3 + 2i
+typeof(z)
+
+# Character
+a <- "h"
+typeof(a)
+b <- 'k'
+typeof(b)
+
+# Logical
+c <- T
+typeof(c)
+d <- TRUE
+typeof(d)
