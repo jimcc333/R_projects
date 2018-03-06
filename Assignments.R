cd26944f2ec1e8e599ba473e8eb9cda62f62c4c5
Cem <jimcc333@gmail.com>
2018-03-06 13:14:26 -0600

    Adds script to survey variable assignment and calculations

---
+++ Assignments.R
@@ -0,0 +1,22 @@
+# Assign some values
+A <- 10
+B <- 5
+
+C <- A + B
+
+var1 <- 2.5
+var2 <- 4
+
+greeting <- "Hello"
+name <- "Bobby"
+
+# Some numerical calculaions
+result <- var1 / var2
+result
+
+answer <- sqrt(A)
+answer
+
+# Text calculation
+message <- paste(greeting, name)
+message
