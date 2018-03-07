e560ca4e9c2a612039859bce66da1845eb748428
Cem <jimcc333@gmail.com>
2018-03-07 13:05:36 -0600

    Adds interesting visualizations on the basketball data

--- BasketballPlotter.R
+++ BasketballPlotter.R
@@ -28,9 +28,20 @@ Data <- MinutesPlayed[1,,drop=F]
 matplot(t(Data), type="b", pch=15:18, col=c(1:4,6))
 legend("bottomleft", inset=0.01, legend = Players[1], pch=15:18, col=c(1:4,6))
 
+# Uses functions from PlotFunctions.R
+RowPlotter(Salary)
+RowPlotter(Salary / Games)
+RowPlotter(Salary / FieldGoals)
 
+# In-Game metrics
+RowPlotter(FieldGoals / Games)
+RowPlotter(FieldGoals / FieldGoalAttempts)
+RowPlotter(FieldGoalAttempts / Games)
+RowPlotter(Points / Games)
 
-
-
-
+# Interesting observations
+RowPlotter(MinutesPlayed / Games)
+RowPlotter(Games)
+RowPlotter(FieldGoals / MinutesPlayed)
+RowPlotter(Points / FieldGoals)
 
