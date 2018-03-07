d4f9a2cae8b7a3b2dd5c057c1105b3b2f642871a
Cem <jimcc333@gmail.com>
2018-03-07 12:52:41 -0600

    Adds new plot function

--- BasketballPlotter.R
+++ BasketballPlotter.R
@@ -22,3 +22,15 @@ legend("bottomleft", inset=0.01, legend = Players, pch=15:18, col=c(1:4,6))
 # Plot field accuracy trend
 matplot(t(FieldGoals/FieldGoalAttempts), type="b", pch=15:18, col=c(1:4,6))
 legend("bottomleft", inset=0.01, legend = Players, pch=15:18, col=c(1:4,6))
+
+# Plot minutes played of Kobe
+Data <- MinutesPlayed[1,,drop=F]
+matplot(t(Data), type="b", pch=15:18, col=c(1:4,6))
+legend("bottomleft", inset=0.01, legend = Players[1], pch=15:18, col=c(1:4,6))
+
+
+
+
+
+
+
